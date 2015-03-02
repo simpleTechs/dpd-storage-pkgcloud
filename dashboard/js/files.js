dd2e78bca29fa5dbe12796b8939a0ab37c079a49
Fabian Off <fabian@simpletechs.net>
2015-03-02 23:26:38 +0100

    support deleting files from the backend

--- dashboard/js/files.js
+++ dashboard/js/files.js
@@ -1,17 +1,33 @@
 (function() {
-    var $tmpl = $('#files li.component-template');
-    dpd('f').get({action: 'list'}).then(function(files) {
-        var $fileRows = [];
-        files.forEach(function(file) {
-            var $fileRow = $tmpl.clone();
-            $fileRow.show().find('a.file-link').attr({
-                href: '/' + Context.resourceId + '/' + file.name,
-                target: '_blank'
-            }).text(file.name);
+    function refresh() {
+        var $tmpl = $('#files li.component-template');
+        dpd(Context.resourceId).get({action: 'list'}).then(function(files) {
+            var $fileRows = [];
+            files.forEach(function(file) {
+                if(file.name[file.name.length-1] === '/' && file.size === 0) return;
 
-            $fileRows.push($fileRow);
+                var $fileRow = $tmpl.clone().show();
+                $fileRow.find('a.file-link').attr({
+                    href: '/' + Context.resourceId + '/' + file.name
+                }).text(file.name);
+                $fileRow.find('a.file-delete').click(function() {
+                    $.get('/' + Context.resourceId + '/' + file.name + '?_method=DELETE').then(function() {
+                        setTimeout(function() {
+                            alert('File was deleted!');
+                        }, 100);
+                    }, function(err) {
+                        console.log(arguments);
+                        alert('An error happened when deleting this file!');
+                    }).always(refresh);
+                });
+                $fileRows.push($fileRow);
+            });
+
+            var $list = $('#files ul.component-list');
+            $list.html('').append($tmpl.get(0));
+            $($fileRows.map(function(value) { return value.get(0); })).appendTo($list);
         });
+    }
 
-        $($fileRows.map(function(value) { return value.get(0) })).appendTo('#files ul.component-list');
-    });
+    refresh();
 })();
